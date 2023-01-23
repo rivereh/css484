b9c433ebba3569598ddac3231ffdc9a94129143a
River <riverehill@gmail.com>
2023-01-23 12:24:42 -0800

    Add checkboxes to images

--- assignment1/ImageViewer.py
+++ assignment1/ImageViewer.py
@@ -1,23 +1,29 @@
 # ImageViewer.py
 # Program to start evaluating an image in python
 from tkinter import *
-import math, os, sys, subprocess
+import math
+import os
+import sys
+import subprocess
 from PixInfo import PixInfo
 
 # for inspecting images on mac or pc
+
+
 def open_file(filename):
     if sys.platform == "win32":
         os.startfile(filename)
     else:
-        opener ="open" if sys.platform == "darwin" else "xdg-open"
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
         subprocess.call([opener, filename])
 
+
 class ImageViewer(Frame):
-    
+
     def __init__(self, master, pixInfo, resultWin):
         Frame.__init__(self, master)
-        self.master    = master
-        self.pixInfo   = pixInfo
+        self.master = master
+        self.pixInfo = pixInfo
         self.resultWin = resultWin
         self.colorCode = pixInfo.get_colorCode()
         self.intenCode = pixInfo.get_intenCode()
@@ -28,83 +34,83 @@ class ImageViewer(Frame):
         # Image size for formatting.
         self.xmax = pixInfo.get_xmax()
         self.ymax = pixInfo.get_ymax()
-        
+
         # Create Main frame.
         mainFrame = Frame(master)
         mainFrame.pack()
-        
+
         # Create Picture chooser frame.
         listFrame = Frame(mainFrame)
         listFrame.pack(side=LEFT)
-        
+
         # Create Control frame.
         controlFrame = Frame(mainFrame)
         controlFrame.pack(side=RIGHT)
-        
+
         # Create Preview frame.
         previewFrame = Frame(mainFrame, width=self.xmax+45, height=self.ymax)
         previewFrame.pack_propagate(0)
         previewFrame.pack(side=RIGHT)
-        
+
         # Create Results frame.
         resultsFrame = Frame(self.resultWin)
         resultsFrame.pack(side=BOTTOM)
         self.canvas = Canvas(resultsFrame)
         self.resultsScrollbar = Scrollbar(resultsFrame)
         self.resultsScrollbar.pack(side=RIGHT, fill=Y)
-        
+
         # Layout Picture Listbox.
         self.listScrollbar = Scrollbar(listFrame)
         self.listScrollbar.pack(side=RIGHT, fill=Y)
-        self.list = Listbox(listFrame, 
-            yscrollcommand=self.listScrollbar.set, 
-            selectmode=BROWSE, 
-            height=10)
+        self.list = Listbox(listFrame,
+                            yscrollcommand=self.listScrollbar.set,
+                            selectmode=BROWSE,
+                            height=10)
         for i in range(len(self.imageList)):
             self.list.insert(i, self.imageList[i].filename)
         self.list.pack(side=LEFT, fill=BOTH)
         self.list.activate(1)
         self.list.bind('<<ListboxSelect>>', self.update_preview)
         self.listScrollbar.config(command=self.list.yview)
-         
+
         # Layout Controls.
-        button = Button(controlFrame, text="Inspect Pic", 
-            fg="red", padx = 10, width=10, 
-            command=lambda: self.inspect_pic(
-                self.list.get(ACTIVE)))
+        button = Button(controlFrame, text="Inspect Pic",
+                        fg="red", padx=10, width=10,
+                        command=lambda: self.inspect_pic(
+                            self.list.get(ACTIVE)))
         button.grid(row=0, sticky=E)
-        
-        self.b1 = Button(controlFrame, text="Color-Code", 
-            padx = 10, width=10, 
-            command=lambda: self.find_distance(method='CC'))
+
+        self.b1 = Button(controlFrame, text="Color-Code",
+                         padx=10, width=10,
+                         command=lambda: self.find_distance(method='CC'))
         self.b1.grid(row=1, sticky=E)
-        
-        b2 = Button(controlFrame, text="Intensity", 
-            padx = 10, width=10, 
-            command=lambda: self.find_distance(method='inten'))
+
+        b2 = Button(controlFrame, text="Intensity",
+                    padx=10, width=10,
+                    command=lambda: self.find_distance(method='inten'))
         b2.grid(row=2, sticky=E)
-        
+
         self.resultLbl = Label(controlFrame, text="Results:")
         self.resultLbl.grid(row=3, sticky=W)
-        
+
         # Layout Preview.
         self.selectImg = Label(previewFrame, image=self.photoList[0])
         self.selectImg.pack()
-    
+
     # Event "listener" for listbox change.
     def update_preview(self, event):
         i = self.list.curselection()[0]
         self.selectImg.configure(
             image=self.photoList[int(i)])
-    
+
     # Find the Manhattan Distance of each image and return a
     # list of distances between image i and each image in the
-    # directory uses the comparison method of the passed 
+    # directory uses the comparison method of the passed
     # binList
     def find_distance(self, method):
-        distances = {} # index, distance
+        distances = {}  # index, distance
         i = self.list.curselection()[0]
-        
+
         # get pixel count of selected image
         imageIW, imageIH = self.imageList[i].size
         imageIPixelCount = imageIW * imageIH
@@ -114,25 +120,26 @@ class ImageViewer(Frame):
             # skip selected image from being displayed in grid
             if i == j:
                 continue
-            
+
             # get pixel count of image being compared
             imageJW, imageJH = imageJ.size
             imageJPixelCount = imageJW * imageJH
 
             if method == 'inten':
-                distance = sum(abs((val1/imageIPixelCount) - (val2/imageJPixelCount)) 
-                    for val1, val2 in zip(pixInfo.get_intenCode()[i],pixInfo.get_intenCode()[j]))
+                distance = sum(abs((val1/imageIPixelCount) - (val2/imageJPixelCount))
+                               for val1, val2 in zip(pixInfo.get_intenCode()[i], pixInfo.get_intenCode()[j]))
 
             elif method == 'CC':
                 distance = sum(abs((val1/imageIPixelCount) - (val2/imageJPixelCount))
-                    for val1, val2 in zip(pixInfo.get_colorCode()[i],pixInfo.get_colorCode()[j]))
+                               for val1, val2 in zip(pixInfo.get_colorCode()[i], pixInfo.get_colorCode()[j]))
 
             # add computed distance to distances
             distances[j] = distance
 
         # sort distances
-        distances = {k: v for k, v in sorted(distances.items(), key=lambda item: item[1])}
-        
+        distances = {k: v for k, v in sorted(
+            distances.items(), key=lambda item: item[1])}
+
         self.update_results(distances)
 
     # Update the results window with the sorted results.
@@ -140,22 +147,23 @@ class ImageViewer(Frame):
         cols = int(math.ceil(math.sqrt(len(sortedTup))))
         fullsize = (0, 0, (self.xmax*cols), (self.ymax*cols))
 
-        # Initialize the canvas with dimensions equal to the 
+        # Initialize the canvas with dimensions equal to the
         # number of results.
         self.canvas.delete(ALL)
-        self.canvas.config( 
-            width=self.xmax*cols, 
-            height=self.ymax*cols/2, 
+        self.canvas.config(
+            width=self.xmax*cols,
+            height=self.ymax*cols/2,
             yscrollcommand=self.resultsScrollbar.set,
             scrollregion=fullsize)
         self.canvas.pack()
         self.resultsScrollbar.config(command=self.canvas.yview)
-        
+
         photoRemain = []
 
         # retrieve images from self.photoList using keys from sortedTup
         for key in sortedTup.keys():
-            photoRemain.append((self.imageList[key].filename, self.photoList[key]))
+            photoRemain.append(
+                (self.imageList[key].filename, self.photoList[key]))
 
         rowPos = 0
         while photoRemain:
@@ -164,27 +172,48 @@ class ImageViewer(Frame):
             colPos = 0
 
             for (filename, img) in photoRow:
-                link = Button(self.canvas, image=img)
-                link.image = img
 
-                handler = lambda f=filename: self.inspect_pic(f)
+                def handler(f=filename): return self.inspect_pic(f)
+
+                relevantBtn = Checkbutton(self.canvas, text=img,
+                                          onvalue=1, offvalue=0, command=lambda v=img: display_input(v),
+                                          anchor=S)
+                relevantBtn.pack()
+                link = Button(self.canvas, image=img, anchor=N)
+                link.image = img
                 link.config(command=handler)
-                link.pack(side=LEFT, expand=YES)
+                link.pack(side=TOP, expand=YES)
+
+                # create picture windows
                 self.canvas.create_window(
-                    colPos, 
-                    rowPos, 
+                    colPos,
+                    rowPos,
                     anchor=NW,
-                    window=link, 
-                    width=self.xmax, 
+                    window=link,
+                    width=self.xmax,
+                    height=self.ymax - 20)
+
+                # create relevant buttons
+                self.canvas.create_window(
+                    colPos,
+                    rowPos,
+                    anchor=NW,
+                    window=relevantBtn,
+                    width=self.xmax,
                     height=self.ymax)
+
                 colPos += self.xmax
-                
+
             rowPos += self.ymax
-    
+
     # Open the picture with the default operating system image
     # viewer.
     def inspect_pic(self, filename):
         open_file(filename)
+
+
+def display_input(img):
+    print(img)
 
 
 # Executable section.
--- assignment1/PixInfo.py
+++ assignment1/PixInfo.py
@@ -2,7 +2,10 @@
 # Program to start evaluating an image in python
 
 from PIL import Image, ImageTk
-import glob, os, re
+import glob
+import os
+import re
+
 
 class PixInfo:
     def __init__(self, master):
@@ -20,8 +23,8 @@ class PixInfo:
         infiles = [os.path.basename(p) for p in infiles]
 
         for infile in infiles:
-            
-            # Add each image (for evaluation) into a list, 
+
+            # Add each image (for evaluation) into a list,
             # and a Photo from the image (for the GUI) in a list.
             # print(infile)
             im = Image.open(f'images/{infile}')
@@ -43,16 +46,18 @@ class PixInfo:
             self.imageList.append(im)
             self.photoList.append(photo)
 
+        self.ymax = 96
+
         # Create a list of pixel data for each image and add it
         # to a list.
         for im in self.imageList[:]:
-            
+
             pixList = list(im.getdata())
             CcBins, InBins = self.encode(pixList)
             self.colorCode.append(CcBins)
             self.intenCode.append(InBins)
 
-    # Bin function returns an array of bins for each 
+    # Bin function returns an array of bins for each
     # image, both Intensity and Color-Code methods.
     def encode(self, pixlist):
 
@@ -63,7 +68,7 @@ class PixInfo:
 
         for pixel in pixlist:
             r, g, b = pixel
-            
+
             # intensity calculation
             intensity = (0.299 * r) + (0.587 * g) + (0.114 * b)
             binNumI = int(intensity // 10) - 1
@@ -83,18 +88,18 @@ class PixInfo:
     # Accessor functions:
     def get_imageList(self):
         return self.imageList
-    
+
     def get_photoList(self):
         return self.photoList
-    
+
     def get_xmax(self):
         return self.xmax
-    
+
     def get_ymax(self):
         return self.ymax
-    
+
     def get_colorCode(self):
         return self.colorCode
-        
+
     def get_intenCode(self):
         return self.intenCode
